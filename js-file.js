c768f75a371a92d7a763242cb8756e94f72dea9a
Ronan Gleeson <ronangleeson11@gmail.com>
2025-12-29 20:36:21 -0500

    Add hover changes grid color

--- js-file.js
+++ js-file.js
@@ -2,5 +2,14 @@ grid = document.querySelector("#grid");
 for (let i = 0; i < 16 * 16; i++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
+    cell.id = `c${i}`;
     grid.append(cell);
 }
+
+grid.addEventListener("mouseover", (event) => {
+    let id = event.target.id;
+    let cell = document.querySelector(`#${id}`);
+    if (id.charAt(0) == "c") {
+        cell.style.backgroundColor = "red";
+    }
+});
