a629b407c5ea9c5c120b2bee2380379f3ee99766
Ronan Gleeson <ronangleeson11@gmail.com>
2025-12-29 19:47:08 -0500

    Add 16 x 16 grid

--- js-file.js
+++ js-file.js
@@ -1,4 +1,6 @@
-body = document.querySelector("body");
-p = document.createElement("p");
-p.textContent = "test";
-body.append(p);
+grid = document.querySelector("#grid");
+for (let i = 0; i < 16 * 16; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    grid.append(cell);
+}
