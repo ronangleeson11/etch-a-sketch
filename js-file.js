eadca821feacccbc72c5524165a53462426861c4
Ronan Gleeson <ronangleeson11@gmail.com>
2025-12-29 20:45:45 -0500

    Add reset button

--- js-file.js
+++ js-file.js
@@ -13,3 +13,21 @@ grid.addEventListener("mouseover", (event) => {
         cell.style.backgroundColor = "red";
     }
 });
+
+reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    let size = prompt("Enter size of new grid");
+    if (size > 100) {
+        alert("Too big, please enter a size <= 100");
+    } else {
+        grid.remove();
+        grid = document.createElement("div");
+        for (let i = 0; i < size; i++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.id = `c${i}`;
+            grid.append(cell);
+        }
+        document.querySelector("body").append(grid);
+    }
+})
